90569e0478e25d43c0b679aed0c8ae2ec0690a99
Odum <kristianodum@gmail.com>
2021-09-24 09:48:46 +0200

    ignore

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+    println("This is Kotlin; thank god for that :)")
+
+    // Try adding program arguments at Run/Debug configuration
+    println("Program arguments: ${args.joinToString()}")
+}
