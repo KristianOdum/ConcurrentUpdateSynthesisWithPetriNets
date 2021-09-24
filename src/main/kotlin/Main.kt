b168b9a3c5d56ec107f9d4253232a966d3974a37
Odum <kristianodum@gmail.com>
2021-09-24 09:47:02 +0200

    .gitignore is now working

--- src/main/kotlin/Main.kt
+++
@@ -1,7 +0,0 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-    println("This is Kotlin; thank god for that :)")
-
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
-}
