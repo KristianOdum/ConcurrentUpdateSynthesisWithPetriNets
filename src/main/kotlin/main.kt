48c353b57a6ca6923f5db39daeb2906f4f17021c
Odum <kristianodum@gmail.com>
2021-10-21 09:20:34 +0200

    NFA graphing works

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -8,14 +8,12 @@ fun main() {
     val usm = updateSynthesisModelFromJsonText(jsonText)
     val nfa = generateNFAFromUSM(usm)
 
-    graphOfNFA(nfa)
-
     val pg = generatePetriGameModelFromUpdateSynthesisNetwork(usm)
     addGraphicCoordinatesToPG(pg)
     generatePnmlFileFromPetriGame(pg, "output.pnml")
 
-    val verifier = Verifier("verifypn-linux64", "ConcurrentRoutingUpdate.pnml")
-    bisectionSearch(verifier, "tempQuery.q", 50)
+//    val verifier = Verifier("verifypn-linux64", "ConcurrentRoutingUpdate.pnml")
+//    bisectionSearch(verifier, "tempQuery.q", 50)
 
 }
 
