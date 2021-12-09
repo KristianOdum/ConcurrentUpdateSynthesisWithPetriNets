a4d0f0e4ed6206756aa98b9383144bb6594f5d82
Slorup <nicklas.slorup@yahoo.dk>
2021-12-09 15:40:07 +0100

    Nondet benchmark gen files

---
+++ Generate_NonDet_Zoo_Json.py
@@ -0,0 +1,23 @@
+import utils.JsonNonDetBuilder as JB
+import time,os
+import random
+
+### Generates a problem and writes it as json
+def generate(indir, network, outdir, scale):
+    gml = indir + network + ".gml"
+    json = f"{outdir}_json/{network}_{scale}.json"
+    JB.jsonbuilder(gml, json, scale)
+
+def generate_all(indir, outdir, scale):
+    start = time.time()
+    cnt = 0
+    for f in os.listdir(indir):
+        for i in range(1, scale):
+            generate(indir + "/", f[:-4], outdir, i)
+            cnt += 1
+    print("Operation done in: {} seconds".format((str(time.time()-start))[:5]))
+    print(f"Succesfully written {cnt} files.")
+
+
+#generate("data/gml/", "TataNld", "data/zoo3", 4)
+generate_all("data/gml", "data/zooND_", 5)
